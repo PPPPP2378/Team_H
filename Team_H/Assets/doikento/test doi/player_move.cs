39be48582124dd15beae91d43d441dd3af573d23
DoiKento <h1202519003@hiratagakuen.onmicrosoft.com>
2025-10-20 14:09:25 +0900

    耕す　水やり　種植えを追加

--- Team_H/Assets/doikento/test doi/player_move.cs
+++ Team_H/Assets/doikento/test doi/player_move.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class player_move : MonoBehaviour
 {
     private float speed = 0.01f;//プレイヤーの移動速度
+    private Collider2D currentTarget; // 今触れているオブジェクトを記録する
+
+    [Header("畑のスプライト差し替え用")]
+    [SerializeField] private Sprite plowedSprite;  // 耕した後の見た目
+    [SerializeField] private Sprite wateredSprite; // 水やり後の見た目
+    [SerializeField] private Sprite seedSprite;　　//種を植えた後の見た目
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -37,20 +43,46 @@ public class player_move : MonoBehaviour
 
         transform.position = position;
 
-        // 現在位置のオブジェクトを取得
-        Collider2D hit = Physics2D.OverlapPoint(transform.position);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentTarget != null)
+            {
+                SpriteRenderer sr = currentTarget.GetComponent<SpriteRenderer>();
+                if (sr == null) return; // SpriteRendererがない場合は何もしない
 
-        if (hit != null)
-        {
-            Debug.Log("現在の位置にあるオブジェクトのタグ: " + hit.tag);
-        }
-        else
-        {
-            Debug.Log("現在の位置には何もありません");
+                if (currentTarget.CompareTag("Plow"))
+                {
+                    Debug.Log("畑を耕す");
+                    sr.sprite = plowedSprite; // グラフィック変更
+                    currentTarget.tag = "Plowed"; // タグも変更
+                }
+                else if (currentTarget.CompareTag("Plowed"))
+                {
+                    Debug.Log("畑に水やりした");
+                    sr.sprite = wateredSprite; // グラフィック変更
+                    currentTarget.tag = "Moist_Plowe"; // タグも変更
+                }
+                else if (currentTarget.CompareTag("Moist_Plowe"))
+                {
+                    Debug.Log("畑に種を植えた");
+                    sr.sprite = seedSprite; // グラフィック変更
+                    currentTarget.tag = "Seed"; // タグも変更
+                }
+            }
         }
     }
     void OnTriggerStay2D(Collider2D other)
     {
-        Debug.Log("接触中のタグ: " + other.tag);
+        //Debug.Log("接触中のタグ: " + other.tag);
+        currentTarget = other; // 現在触れているオブジェクトを記録
+    }
+
+    // 離れたときに解除
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (currentTarget == other)
+        {
+            currentTarget = null;
+        }
     }
 }
