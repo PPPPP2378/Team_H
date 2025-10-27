eb9725eac9cc118f7ab10bb6d2f7724550978b70
OKUNORYOTA <h1202519012@hiratagakuen.onmicrosoft.com>
2025-10-27 09:03:12 +0900

    学校PC

--- Team_H/Assets/OkunoRyota/PlayerCont.cs
+++ Team_H/Assets/OkunoRyota/PlayerCont.cs
@@ -1,7 +1,11 @@
+
 using UnityEngine;
 
-public class PlayerCont1 : MonoBehaviour
+
+public class PlayerCont : MonoBehaviour
 {
+    public float speed = 0.5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,12 @@ public class PlayerCont1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput) * speed * Time.deltaTime;
+
+        transform.position += movement;
     }
 }
